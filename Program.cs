95b902415b4661f842d726298feab9d8a8f7f1cb
Abdullah Bisultanov <abd_bisultanov@mail.ru>
2020-11-09 22:46:13 +0600

    realease v1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_3_MSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, s;  
+
+            Console.WriteLine("---------------------Программа возведение в степень-----------------------------");
+            Console.WriteLine("--------------------------------------------------------------------------------");
+
+            Console.WriteLine("Введите a");
+            s = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите b");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            s = System.Math.Pow(s, a);    
+            Console.WriteLine("Степень = " + s);    
+            
+
+            Console.Read();   
+
+        }
+    }
+}
